349742871fa1173bb582287332caeba6d1ac5301
himanshu goyal <himanshu.goyal@outlook.com>
2018-12-01 16:02:29 -0600

    Update youTubeComments.py

--- src/youTubeComments.py
+++ src/youTubeComments.py
@@ -1,13 +1,18 @@
 from apiclient.discovery import build
-from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import  pandas as pd
+import sys
+import string
+import re
+from sys import argv
 # DF TO EXCEL
 from pandas import ExcelWriter
 
-DEVELOPER_KEY = ""
+DEVELOPER_KEY = "AIzaSyBrdaH0BNdxkWvu4y2ji8rQiWmGyQiv6Aw"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+
+
 
 
 def youtube_search(q, max_results=50, order="relevance", token=None, location=None, location_radius=None):
@@ -32,61 +37,69 @@ def youtube_search(q, max_results=50, order="relevance", token=None, location=No
             response = youtube.videos().list(
                 part='statistics, snippet',
                 id=search_result['id']['videoId']).execute()
-            if response['items'][0]['statistics']['viewCount'] > '100000':
+            if response['items'][0]['statistics']['viewCount'] > '1000000':
                 videoId.append(search_result['id']['videoId'])
-    print(videoId)
+    print(len(videoId))
     return videoId
-
-
-def geo_query(video_id):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-                    developerKey=DEVELOPER_KEY)
-
-    video_response = youtube.videos().list(
-        id=video_id,
-        part='snippet, recordingDetails, statistics'
-
-    ).execute()
-
-    return video_response
-
 
 
 def get_comment_threads(searchName):
     outputDF = pd.DataFrame([])
     videoIds= youtube_search(searchName)
-    for id in videoIds:
+    #videoIds = ['PYN4H0JXBJc']
+    for video_id in videoIds:
         youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                         developerKey=DEVELOPER_KEY)
         results = youtube.commentThreads().list(
             part="snippet",
-            videoId=id,
+            videoId=video_id,
             textFormat="plainText"
         ).execute()
 
         for item in results["items"]:
             comment = item["snippet"]["topLevelComment"]
-            author = comment["snippet"]["authorDisplayName"]
+
             createdTime = comment["snippet"]["publishedAt"]
             text = comment["snippet"]["textDisplay"]
-            #print("Comment by %s: at %s: %s" % (author, createdTime,text))
-            outputDF = outputDF.append(pd.DataFrame({'videoID': id, 'CreateTimeStamp': createdTime, 'Comment': text}, index=[0]), ignore_index=True)
-    #print(outputDF)
+            cleanText = cleanComment(text)
+            # print("Comment by %s: at %s: %s" % (author, createdTime,text))
+            outputDF=outputDF.append(pd.DataFrame({'videoID': video_id, 'CreateTimeStamp': createdTime, 'Comment': cleanText, 'Type': 'Comment'}, index=[0]),ignore_index=True)
+
+        for item1 in results["items"]:
+            results1 = youtube.comments().list(
+                part="snippet",
+                parentId=item1['id'],
+                textFormat="plainText"
+            ).execute()
+
+            for item2 in results1["items"]:
+                text1 = item2["snippet"]["textDisplay"]
+                cleanText1 = cleanComment(text1)
+                createdTime1 = item2["snippet"]["publishedAt"]
+                outputDF=outputDF.append(pd.DataFrame({'videoID': video_id, 'CreateTimeStamp': createdTime1, 'Comment': cleanText1, 'Type': 'Reply'}, index=[0]),ignore_index=True)
+        print(video_id)
     return outputDF
 
+def cleanComment(text):
+    text = text.lower()
+    text = re.sub(r'https?:\/\/.\/\w', '', text)
+    text = re.sub('@[^\s]+','',text)
+    text = re.sub(r'\&\w*;', '', text)
+    text = re.sub(r'\$\w*', '', text)
+    text = re.sub(r'#\w*', '', text)
+    text = re.sub(r'\b\w{1,2}\b', '', text)
+    text = re.sub(r'\s\s+', ' ', text)
+    text = re.sub(r'\n', ' ', text)
+    text = re.sub(r'\r', ' ', text)
+    text = ''.join(r for r in text if r <= '\uFFFF')
+    return text
 
 def main():
-    SearchString = 'Eminem - Not Afraid'
+    SearchString = sys.argv[1]
+    #SearchString = 'Samsung VR HeadSet'
     comments = get_comment_threads(searchName=SearchString)
-    writer = ExcelWriter('Comments.xlsx')
-    comments.to_excel(writer, 'Sheet1')
-    writer.save()
-    #youtube_search(q='the family vacation sit')
-    #print(comments)
+    comments.to_csv("SamsungVRHeadSet.csv", sep='\t', index=False)
 
-#The tonight show
-#samsung vr headset
-#
 
 if __name__ == "__main__":
     main()
