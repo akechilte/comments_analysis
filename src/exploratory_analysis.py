a170d58a5414e902fabe6c8f46182e0eeac26f8a
vbhoyar <vbhoyar@iu.edu>
2018-12-06 22:01:43 -0600

    'exploratory_analysis

--- src/exploratory_analysis.py
+++ src/exploratory_analysis.py
@@ -13,9 +13,11 @@ from matplotlib_venn import venn2
 
 
 
-def SENTIMENT():
+def SENTIMENT(filepath ):
+    
+    path = filepath +'\\video_comments_analysis_sentiments.csv'
     ''' Read the file data '''
-    df = pd.read_csv("video_comments_analysis_sentiments.csv", sep='\t' ,names = ["Comment", "CreateTimeStamp", "Type", "videoID","videoTitle", "FNAME", "SENTIMENT_RF", "SENTIMENT_KN"])
+    df = pd.read_csv(path, sep='\t' ,names = ["Comment", "CreateTimeStamp", "Type", "videoID","videoTitle", "FNAME", "SENTIMENT_RF", "SENTIMENT_KN"])
     df = df.iloc[1:]
     
     ''' Plot Comments Count chart '''
@@ -30,64 +32,57 @@ def SENTIMENT():
     fig1.savefig('Comment_count_by_file.png')
        
        
-    ''' Random Forest data analysis'''
+    ''' Random Forest and KNeighbors Sentiment data analysis'''
      
-    sns.set_style('white')
-    fig2 = plt.figure(figsize=(12,8))
-    df1 = df.groupby(['videoID']).Comment.agg('count').to_frame('vidiocount').reset_index().sort_values(by='vidiocount', ascending=False).head(10)   
-    df2 = pd.merge(df, df1, on='videoID', how='inner') 
-    df2['SENTIMENT_RF'] =  np.where(df2['SENTIMENT_RF'] == '0', 'False', 'True')
-    g =sns.countplot(x='videoID',hue='SENTIMENT_RF',data=df2)
-    #plt.title('Random Forest Classifier Analysis - Top 10 Video')
-    g.set_ylabel('Comment Count')
-    g.set_xlabel('Video ID')
-    g.set_xticklabels(g.get_xticklabels(),rotation=45)
-    fig2.savefig('Top 10 Random Forest.png')
-     
-    '''KNeighbors data analysis '''
-    
-    sns.set_style('white')
-    fig3 = plt.figure(figsize=(12,8))
-    df1 = df.groupby(['videoID']).Comment.agg('count').to_frame('vidiocount').reset_index().sort_values(by='vidiocount', ascending=False).head(10)   
-    df2 = pd.merge(df, df1, on='videoID', how='inner') 
-    df2['SENTIMENT_KN'] =  np.where(df2['SENTIMENT_KN'] == '0', 'False', 'True')
-    g =sns.countplot(x='videoID',hue='SENTIMENT_KN',data=df2)
-    #plt.title('KNeighbors Classifier Analysis - Top 10 Video')
-    g.set_ylabel('Comment Count')
-    g.set_xlabel('Video ID')
-    g.set_xticklabels(g.get_xticklabels(),rotation=45)
-    fig3.savefig('Top 10 KNeighborsClassifier.png')
-      
 
-    ''' venn deigram for true values '''
+    fig3 = plt.figure(figsize=(12,8))
+    df1 = df.groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('Count').reset_index().sort_values(by='Count', ascending=False).head(15) 
+    df2= df[df['SENTIMENT_RF']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('POSITIVE_SENT_RF').reset_index()
+    df3= df[df['SENTIMENT_KN']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('POSITIVE_SENT_KN').reset_index()  
+    df4= df[df['SENTIMENT_RF']=='0'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('NEGATIVE_SENT_RF').reset_index()
+    df5= df[df['SENTIMENT_KN']=='0'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('NEGATIVE_SENT_KN').reset_index()  
+    df7 = pd.merge(df1[['videoID']], df2, on='videoID', how='inner')
+    df7 = pd.merge(df7, df3, on='videoID', how='inner')
+    df7 = pd.merge(df7, df4, on='videoID', how='inner')
+    df7 = pd.merge(df7, df5, on='videoID', how='inner')
+    df7 = df7.set_index('videoID')
+#    print(df7)
+    df7.plot(kind='bar',rot=45,width=.8,figsize=(12,8), color= ('b','g', 'r', 'k'))
+    plt.savefig('Sentiemnt_Analysis.png')
+    
+    
+
+    ''' venn deigram for Sentiment values '''
     
     fig4 = plt.figure(figsize=(12,8))
     df3 = df
-    df3['KN'] =  np.where((df3['SENTIMENT_KN'] == '1') & (df3['SENTIMENT_KN'] == df3['SENTIMENT_RF']), 'True'+'-'+df3['CreateTimeStamp'], 'KN'+'-' + df3['CreateTimeStamp'])
-    df3['RF'] =  np.where((df3['SENTIMENT_RF'] == '1') & (df3['SENTIMENT_RF'] == df3['SENTIMENT_KN']), 'True'+'-'+df3['CreateTimeStamp'], 'RF'+'-' + df3['CreateTimeStamp'])
+    df3['KN'] =  np.where( (df3['SENTIMENT_KN'] == df3['SENTIMENT_RF']), 'True'+'-'+df3['CreateTimeStamp'], 'KN'+'-' + df3['CreateTimeStamp'])
+    df4 = df
+    df4['RF'] =  np.where((df4['SENTIMENT_RF'] == df4['SENTIMENT_KN']), 'True'+'-'+df4['CreateTimeStamp'], 'RF'+'-' + df4['CreateTimeStamp'])
     my_list = df3["KN"].tolist()
 #    print(my_list)
-    my_list2 = df3["RF"].tolist()
+    my_list2 = df4["RF"].tolist()
     venn2([set(my_list), set(my_list2)], set_labels = ('KNeighbors Classifier', 'Random Forest Classifier'))
     #plt.title('KNeighbors and Random Forest True Sentiments')
-    fig4.savefig('Classifier True Sentiment Analysis.png')
+    fig4.savefig('Venn_SentimentAnalysis.png')
     plt.gca().set_axis_on()
     plt.show()
     
-def Spam_data():  
+def Spam_data(filepath):  
     
+    path = filepath +'\\video_comments_analysis_spam.csv'
     ''' Read the file data '''
-    df = pd.read_csv("video_comments_analysis_spam.csv", sep='\t' ,names = ["Comment", "CreateTimeStamp", "Type", "videoID","videoTitle", "FNAME", "SPAM_IND_RF","SPAM_IND_KN"])
+    df = pd.read_csv(path, sep='\t' ,names = ["Comment", "CreateTimeStamp", "Type", "videoID","videoTitle", "FNAME", "SPAM_IND_RF","SPAM_IND_KN"])
     df = df.iloc[1:]
     
     '''SPAM data analysis '''
     
-    df1 = df.groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('Count').reset_index().sort_values(by='Count', ascending=False).head(10) 
-    df2= df[df['SPAM_IND_RF']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('TRUE_SPAM_RF').reset_index()
-    df3= df[df['SPAM_IND_RF']=='0'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('FALSE_SPAM_RF').reset_index()
-    df4= df[df['SPAM_IND_KN']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('TRUE_SPAM_KN').reset_index()  
-    df5= df[df['SPAM_IND_KN']=='0'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('FALSE_SPAM_KN').reset_index()  
-    df7 = pd.merge(df1, df2, on='videoID', how='inner')
+    df1 = df.groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('Count').reset_index().sort_values(by='Count', ascending=False).head(15) 
+    df2= df[df['SPAM_IND_RF']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('SPAM_RF').reset_index()
+    df3= df[df['SPAM_IND_KN']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('SPAM_KN').reset_index()  
+    df4= df[df['SPAM_IND_RF']=='0'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('NON_SPAM_RF').reset_index()
+    df5= df[df['SPAM_IND_KN']=='0'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('Non_SPAM_KN').reset_index()  
+    df7 = pd.merge(df1[['videoID']], df2, on='videoID', how='inner')
     df7 = pd.merge(df7, df3, on='videoID', how='inner')
     df7 = pd.merge(df7, df4, on='videoID', how='inner')
     df7 = pd.merge(df7, df5, on='videoID', how='inner')
@@ -97,28 +92,60 @@ def Spam_data():
     #plt.title('Spam Analysis - Top 10 Video')
     plt.xlabel('Video ID')
     plt.ylabel('Comment Count')
-    plt.savefig('Spam Analysis - Top 10 Video.png')
+    plt.savefig('Spam_Analysis.png')
  
     ''' venn deigram for true values '''
+
     
-    fig6 = plt.figure(figsize=(12,8))
-    df3 = df
-    df3['KN'] =  np.where((df3['SPAM_IND_KN'] == '1') & (df3['SPAM_IND_KN'] == df3['SPAM_IND_RF']), 'True'+'-'+df3['CreateTimeStamp'], 'KN'+'-' + df3['CreateTimeStamp'])
-    df3['RF'] =  np.where((df3['SPAM_IND_RF'] == '1') & (df3['SPAM_IND_RF'] == df3['SPAM_IND_KN']), 'True'+'-'+df3['CreateTimeStamp'], 'RF'+'-' + df3['CreateTimeStamp'])
-    my_list = df3["KN"].tolist()
-#    print(my_list)
-    my_list2 = df3["RF"].tolist()
-    venn2([set(my_list), set(my_list2)], set_labels = ('KNeighbors Classifier', 'Random Forest Classifier'))
-    #plt.title('KNeighbors and Random Forest True Spam')
-    fig6.savefig('Classifier True Spam Analysis.png')
-    plt.gca().set_axis_on()
-    plt.show()
+    fig5 = plt.figure(figsize=(8,8))
+    df10 = df
     
+    conditions = [
+    (df10['SPAM_IND_KN'] == df10['SPAM_IND_RF']),
+    (df10['SPAM_IND_KN'] == '1') & (df10['SPAM_IND_RF'] == '0'),
+    (df10['SPAM_IND_KN'] == '0') & (df10['SPAM_IND_RF'] == '1')]
+    choices = ['Random Forest & KNeighbors', 'KNeighbors', 'Random Forest']
+    df10['SPAM'] = np.select(conditions, choices, default='NA')
+ #   df5 = df5.groupby(['SPAM']).CreateTimeStamp.agg('count').to_frame('Count').reset_index()
+       
+  #  print(df10)
+    df10.SPAM.value_counts().plot(kind='pie',  autopct='%.2f')
+    fig5.savefig('Pie_SPAM.png')
+
+   
+def SENTIMENT_SPAM(filepath):
+    path1 =filepath +'\\video_comments_analysis_sentiments.csv'
+    fig7 = plt.figure(figsize=(12,8))
+    df = pd.read_csv(path1, sep='\t' ,names = ["Comment", "CreateTimeStamp", "Type", "videoID","videoTitle", "FNAME", "SENTIMENT_RF", "SENTIMENT_KN"])
+    df = df.iloc[1:]
+    
+    path2 = filepath +'\\video_comments_analysis_spam.csv'
+    df3 = pd.read_csv(path2, sep='\t' ,names = ["Comment", "CreateTimeStamp", "Type", "videoID","videoTitle", "FNAME", "SPAM_IND_RF","SPAM_IND_KN"])
+    df3 = df3.iloc[1:]
+    
+    df1 = df.groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('Count').reset_index().sort_values(by='Count', ascending=False).head(15) 
+    df2 = df[df['SENTIMENT_KN']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('SENTIMENT').reset_index()  
+    df3 = df3[df3['SPAM_IND_KN']=='1'].groupby(['videoID']).CreateTimeStamp.agg('count').to_frame('SPAM').reset_index()  
+    df4 = pd.merge(df1[['videoID']], df2, on='videoID', how='inner') 
+    df4 = pd.merge(df4, df3, on='videoID', how='inner')  
+    print(df4)
+    df4 = df4.set_index('videoID')
+    df4.plot(kind='bar',rot=45,width=.8,figsize=(12,8))
+    plt.savefig('Sentiment_SPAM_Analysis.png')
+    
+# =============================================================================
 def main():
     
+    ''' provide file path'''
+    print("Plese provide the output file path (like c:\\Users \\USER\\) :")
+    filepath = input()
     ''' Call functions from main function '''
-    Spam_data()
-    SENTIMENT()
+    SENTIMENT(filepath )
+    Spam_data(filepath)
+    SENTIMENT_SPAM(filepath)
+    
+    
+
     
          
    
